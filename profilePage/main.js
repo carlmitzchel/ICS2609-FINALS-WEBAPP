3141b8c01952cf88a5eb727dc4bd88f573a4bcd0
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2023-05-17 02:00:04 +0800

    Calendar

--- profilePage/main.js
+++ profilePage/main.js
@@ -1,3 +1,50 @@
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "bi bi-x" : "bi bi-list";
+};
+
+// function btnClicked() {
+//   document.getElementById("section_2").scrollIntoView({ behavior: "smooth" });
+// }
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelector(".menu li");
+  const selected = dropdown.querySelector(".selected");
+
+  select.addEventListener("click", () => {
+    alert(
+      "WIP: Scroll down while hovering in the dropdown to access other inputs"
+    );
+    select.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-rotate");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
+    });
+  });
+});
+
 const editBtn = document.getElementById("edit-btn");
 const updateBtn = document.getElementById("update-btn");
 const nameField = document.getElementById("name");
